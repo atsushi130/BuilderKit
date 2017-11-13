9a500a1ae013aaa33e2998e196b9d398c2320dcb
atsushi130 <atsushi_main@outlook.jp>
2017-11-13 09:11:04 +0900

    refactoring

--- src/main/kotlin/extension/PropertySpec+.kt
+++ src/main/kotlin/extension/PropertySpec+.kt
@@ -17,7 +17,6 @@
 package extension
 
 import builderkit.ClassInformation
-import builderkit.PropertyType
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.PropertySpec
 
@@ -29,11 +28,6 @@ import com.squareup.kotlinpoet.PropertySpec
  */
 internal fun PropertySpec.Companion.defines(classInformation: ClassInformation, mutable: Boolean, modifier: KModifier): List<PropertySpec> {
     return classInformation.properties.map { (name, property) ->
-        val propertySpec = when (property) {
-            is PropertyType.Normal  -> PropertySpec.builder(name, property.rawType)
-            is PropertyType.Generic -> PropertySpec.builder(name, property.typeVariableName)
-        }
-
-        propertySpec.initializer(name).mutable(mutable).addModifiers(modifier).build()
+        PropertySpec.builder(name, property.typeVariableName).initializer(name).mutable(mutable).addModifiers(modifier).build()
     }
 }
